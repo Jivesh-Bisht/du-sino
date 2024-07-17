5e26aa87170cfab05389e4174345cbeec3a5e42d
Jivesh-Bisht <110481740+Jivesh-Bisht@users.noreply.github.com>
2024-07-17 22:46:45 +0530

    added russian roulette

---
+++ games/russian_roulette.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+def roulette(amount:int,uid:int) -> bool:
+    """
+    odd position: player
+    even position:computer
+    """
+    
+    choice = random.choice([1,2,3,4,5,6],[23,9,23,11,23,11])
+    if choice in [2,4,6]:
+        return True
+    else:
+        False
+    
+"add concept of removing/adding currency"
